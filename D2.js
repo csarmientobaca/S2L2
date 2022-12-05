c791c5f244e507bb34ab40b730600f149fa90fc7
Carlos Sarmiento <c.sarmientobaca@gmail.com>
2022-12-05 15:13:57 +0100

    finito

--- D2.js
+++ D2.js
@@ -13,23 +13,55 @@ REGOLE
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
+abc = "string" //--> Le strigns sono alphanumeriche ossia della a alla z e da 0 a 9, sono usate con le virgolette, 
+console.log(abc)
+
+
+num = 09345//   --> Datatype numerico, sono i numeri da 0 a 9 e posso essere infinity.
+console.log(num)
+
+bool = true
+console.log(bool)//-->Solo puo essere vero o falso
+console.log(!bool)
+
+vuoto = null    //-->è vuoto, non ha un valore dentro della variabile
+console.log(vuoto)
+
+und = undefined //-->che non ha un valore o non è stato dichiarato
+console.log(und)
+
 /* ESERCIZIO 2
  Crea una variable chiamata "name" e assegna ad essa il tuo nome, sotto forma di stringa.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+let name = "Carlos"
+console.log(name)
+
 
 /* ESERCIZIO 3
  Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+let num1 = 12
+let num2 = 20
+
+somma = num1 + num2
+console.log("Il risultato della somma è: " + somma)
+
 
 /* ESERCIZIO 4
  Crea una variable di nome "x" e assegna ad essa il numero 12.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+
+let x
+x = 12
+
+console.log(x)
+
 
 /* ESERCIZIO 5
   Riassegna un nuovo valore alla variabile "name" già esistente: il tuo cognome.
@@ -38,11 +70,25 @@ REGOLE
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
+name = "Sarmiento"
+console.log(name)
+
+const cognome = name
+
+// cognome = "Zarmiento"
+
+
 /* ESERCIZIO 6
  Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
 */
 
+
 /* SCRIVI QUI LA TUA RISPOSTA */
+sottrazione = x - 4
+console.log("Il risultato della sottrazione è: " + sottrazione)
+
+
+
 
 /* ESERCIZIO 7
  Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
@@ -51,3 +97,7 @@ REGOLE
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+name1 = "john"
+name2 = "Jhon"
+
+console.log("name1 e name2 sono uguali?: " + (name1 === name2))
